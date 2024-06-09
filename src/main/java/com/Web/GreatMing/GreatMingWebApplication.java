e4bfc7476decb1d2cce0b54d3f9badb7100aa254
= <=>
2024-06-10 01:24:50 +0800

    git Init

---
+++ src/main/java/com/Web/GreatMing/GreatMingWebApplication.java
@@ -0,0 +1,13 @@
+package com.Web.GreatMing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatMingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatMingWebApplication.class, args);
+	}
+
+}
