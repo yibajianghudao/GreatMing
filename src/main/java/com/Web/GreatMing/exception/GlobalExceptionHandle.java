a23fe6f0ebe10f6b7aa59a243da8989e3c0e96f5
yibajianghudao <w2027075535@outlook.com>
2024-11-17 23:09:06 +0800

    update GlobalExceptionHandle

--- src/main/java/com/Web/GreatMing/exception/GlobalExceptionHandle.java
+++ src/main/java/com/Web/GreatMing/exception/GlobalExceptionHandle.java
@@ -11,7 +11,8 @@ public class GlobalExceptionHandle {
 
     @ExceptionHandler(Exception.class)
     public Response<?> handleException(Exception e){
-        e.printStackTrace();
+        // e.printStackTrace();
+        System.out.println(e.getMessage());
         return Response.newFail(StringUtils.hasLength(e.getMessage()) ? e.getMessage() : "输出错误信息失败,该错误类型没有实现getMessage方法");
     }
 }
